500c3887ed56dea61a88cc3338c1bb5f72617403
Leonardo Pereira <leonardopereirabvg2018@gmail.com>
2025-09-20 19:41:45 -0300

    Configura Whitenoise para servir arquivos estáticos

--- English_School/settings.py
+++ English_School/settings.py
@@ -116,6 +116,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # ... os outros middlewares
+]
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
